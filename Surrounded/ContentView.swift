43d8b8f202c19387f073a5d3bb806628acd90fb2
Tsungyu Yu <ytyubox@gmail.com>
2020-12-08 11:38:54 +0800

    Initial Commit

---
+++ Surrounded/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Surrounded
+//
+//  Created by 游宗諭 on 2020/12/8.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Surrounded/SurroundedApp.swift
@@ -0,0 +1,17 @@
+//
+//  SurroundedApp.swift
+//  Surrounded
+//
+//  Created by 游宗諭 on 2020/12/8.
+//
+
+import SwiftUI
+
+@main
+struct SurroundedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
