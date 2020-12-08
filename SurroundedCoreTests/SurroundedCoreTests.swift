eda5ff6d4d70f130411fe80c702616c7f677c8c8
Tsungyu Yu <ytyubox@gmail.com>
2020-12-08 14:19:19 +0800

    one line one code can by surround by input block and intent

--- SurroundedCoreTests/SurroundedCoreTests.swift
+++ SurroundedCoreTests/SurroundedCoreTests.swift
@@ -7,11 +7,36 @@
 
 import XCTest
 import SurroundedCore
-
+class Worker {
+    var intent = "    "
+    fileprivate func inputBlock(_ placeholder: String) -> String {
+        return ["<","#", placeholder, "#", ">"].joined()
+    }
+    
+    func surround(placeholder: String = "Group", _ code: String) -> String {
+        let p = inputBlock(placeholder)
+        
+        let output =  """
+            \(p) {
+            \(intent)\(code)
+            }
+            """
+        return output
+    }
+}
 class SurroundedCoreTests: XCTestCase {
 
-    func test() {
-        XCTFail()
+    func testSingleLineCanBeSurroundByInputBlockWith1Intent() {
+        let code = """
+        Text("123")
+        """
+        let sut = Worker()
+        let expect = """
+        <#Group#> {
+            Text("123")
+        }
+        """
+        XCTAssertEqual(sut.surround(code), expect)
     }
 
 }
